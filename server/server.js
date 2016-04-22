bde0c2ce160dc303dc8ec9822a98c5418e38c918
Femke Reunes <femke.reunes@ida-mediafoundry.be>
2016-04-22 11:28:17 +0200

    Allows only access from api gateway

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ let mongoUtil = require("./util/mongoUtil");
 mongoUtil.connect();
 
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header("Access-Control-Allow-Origin", "http://localhost:4000");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
 	next();
